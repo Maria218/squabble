8a2885690e580b25ffe66936b46580c470ccdc86
Maria Situmbeko <mariasitumbeko2@gmail.com>
2021-12-17 01:37:30 +0200

    Login form coming together

--- lib/screens/authentication/login/components/login_form.dart
+++ lib/screens/authentication/login/components/login_form.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:squabble/blocs/authentication/bloc.dart';
 import 'package:squabble/repositories/repositories.dart';
 import 'package:squabble/screens/authentication/login/bloc/bloc_exports.dart';
 
@@ -20,6 +21,7 @@ class _LoginFormState extends State<LoginForm> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   late bool isPasswordObscure;
+  final _formKey = GlobalKey<FormState>();
   User? user;
   late LoginBloc _loginBloc;
 
@@ -68,42 +70,200 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text('THIS IS THE LOGO', style: TextStyle(color: Colors.white),)
-          ],
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state.isSubmitting) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Please wait...'),
+                    CircularProgressIndicator(),
+                  ],
+                ),
               ),
-              autocorrect: false,
-              validator: (_) {
-                return !_loginBloc.state.isEmailValid ? 'Invalid Email' : null;
-              },
-            ),
-            TextFormField(
-              controller: _passwordController,
-              decoration: const InputDecoration(
-                labelText: 'Password',
+            );
+        }
+        if (state.isSuccess) {
+          BlocProvider.of<AuthenticationBloc>(context).add(AuthenticationLoggedIn());
+          Navigator.of(context).popUntil((route) => route.isFirst);
+        }
+        if (state.isFailure) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(
+              SnackBar(
+                content: Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('Login Failure!'),
+                      Icon(Icons.error,size: 30,),
+                    ],
+                  ),
+                ),
+                backgroundColor: Colors.red,
               ),
-              autocorrect: false,
-              validator: (_) {
-                return !_loginBloc.state.isEmailValid ? 'Invalid Email' : null;
-              },
-            ),
-          ],
-        )
-      ],
+            );
+        }
+      },
+      child: BlocBuilder<LoginBloc, LoginState>(
+        builder: (context, state) {
+          return Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: Colors.black,
+            body: SingleChildScrollView(
+              child: Container(
+                height: MediaQuery.of(context).size.height,
+                width: MediaQuery.of(context).size.width,
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Image(
+                        image: AssetImage(
+                          'assets/icon/adaptive_icons/foreground.png'
+                        ),
+                        height: 170,
+                        width: 170,
+                      ),
+                    ),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(15, 60, 15, 10),
+                            child: TextFormField(
+                              controller: _emailController,
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              style: TextStyle(color: Colors.white),
+                              decoration: InputDecoration(
+                                isDense: true,
+                                labelText: 'Email',
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                  // fontSize: 20,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  borderSide: BorderSide(
+                                    color: Colors.white
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  borderSide: BorderSide(
+                                    color: Colors.teal.shade100,
+                                  ),
+                                ),
+                                focusedErrorBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  borderSide: BorderSide(
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                errorBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  borderSide: BorderSide(
+                                    color: Colors.red.shade900,
+                                  ),
+                                )
+                              ),
+                              autofocus: false,
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter your email address';
+                                }
+                                return !state.isEmailValid ? 'Invalid Email' : null;
+                              },
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(15, 10, 15, 5),
+                            child: TextFormField(
+                              controller: _passwordController,
+                              obscureText: isPasswordObscure,
+                              style: TextStyle(color: Colors.white),
+                              autocorrect: false,
+                              autofocus: false,
+                              decoration: InputDecoration(
+                                labelText: 'Password',
+                                labelStyle: TextStyle(
+                                  color: Colors.white,
+                                  // fontSize: 20,
+                                ),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  borderSide: BorderSide(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                focusedErrorBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  borderSide: BorderSide(
+                                    color: Colors.red,
+                                  ),
+                                ),
+                                errorBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0),
+                                  borderSide: BorderSide(
+                                    color: Colors.red.shade900,
+                                  ),
+                                ),
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    isPasswordObscure ? Icons.visibility : Icons.visibility_off,
+                                    color: Colors.white,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      isPasswordObscure = !isPasswordObscure;
+                                    });
+                                  },
+                                ),
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter a strong password!';
+                                }
+                                else if (value.length < 6) {
+                                  return 'Password must be at least 6 characters long';
+                                }
+                                return null;
+                              }
+                            )
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                _onFormSubmitted();
+                              }
+                              return null;
+                            },
+                            child: const Text('Login'),
+                          )
+                        ]
+                      ),
+                    ),
+                  ]
+                )
+              ),
+            )
+          );
+        },
+      ),
     );
   }
 }
